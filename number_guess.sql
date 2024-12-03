9fbc05d48ad11649b78446f193ff1088d0fd6f9a
Nicolas Vo <nicolasvo.jc@gmail.com>
2024-12-03 16:22:37 +0000

    fix: add default worst case best_game

--- number_guess.sql
+++ number_guess.sql
@@ -26,7 +26,7 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.users (
     username character varying(22) NOT NULL,
-    best_game integer NOT NULL,
+    best_game integer DEFAULT 999 NOT NULL,
     num_games integer DEFAULT 1 NOT NULL
 );
 
