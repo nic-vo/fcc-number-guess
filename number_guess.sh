1f13bbc4ad47fd700f46e61591d8297af4f74589
Nicolas Vo <nicolasvo.jc@gmail.com>
2024-12-03 06:06:41 +0000

    feat: lay out pseudocode for flow

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+# Ask for username
+
+# query based on username
+
+# if no existing user, welcome message
+
+# else display statistics
+
+# generate random
+
+# init user guess, guess count
+
+# while user guess !== random || user guess IS NOT int, update count
+
+# when user guess === random
+
+# if no existing user, insert new data
+
+# if user, update num_games and, if necessary, best_game
+
+# echo success message
