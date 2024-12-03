c3944df676cda65da735d3e91128779a8b4ad62f
Nicolas Vo <nicolasvo.jc@gmail.com>
2024-12-03 16:20:55 +0000

    feat: take username input and display results of user query if any

--- number_guess.sh
+++ number_guess.sh
@@ -22,12 +22,20 @@ do
 done
 
 # query based on username
-# USER="$($PSQL "SELECT * FROM users WHERE username='$USERNAME'")"
-
-# if no existing user, welcome message
-
-# else display statistics
-
+USER="$($PSQL "SELECT username,num_games,best_game FROM users WHERE username='$USERNAME'")"
+# if no existing user,
+if [[ -z $USER ]]
+then
+  # welcome message
+  NL_ECHO "Welcome, $USERNAME! It looks like this is your first time here."
+  NL_ECHO "$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")"
+else
+  # else display statistics
+  echo "$USER" | while IFS="|" read USERNAME NUM BEST
+  do
+    NL_ECHO "Welcome back, $USERNAME! You have played $NUM games, and your best game took $BEST guesses."
+  done
+fi
 # generate random
 
 # init user guess, guess count
